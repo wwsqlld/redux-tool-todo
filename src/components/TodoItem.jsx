2df80f100712be9509ba6b661b644fe4399085c3
Vladyslav Bazchenko <60629969+wwsqlld@users.noreply.github.com>
2023-09-24 17:39:00 +0200

    Add files via upload

---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,21 @@
+import { useDispatch } from 'react-redux';
+import {toggleComplete, removeTodo} from '../store/todoSlice';
+
+const TodoItem = ({ id, text, completed }) => {
+  const dispatch = useDispatch();
+
+  return (
+    <li>
+      <input
+        type='checkbox'
+        checked={completed}
+        onChange={() => dispatch(toggleComplete({ id }))}
+      />
+      <span>{text}</span>
+      <span className='delete' onClick={() => dispatch(removeTodo({id}))}>&times;</span>
+    </li>
+  );
+};
+
+export default TodoItem;
+
